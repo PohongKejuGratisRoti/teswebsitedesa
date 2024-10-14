021ebaaedb0ed4189c6867e7aa31c024e90b436e
PohongKejuGratisRoti <arrywahyu0@gmail.com>
2024-10-14 09:16:33 +0700

    tes pertama

---
+++ desacantik/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from './components/pages/Home'
+import './index.css'
+
+function App() {
+  return (
+    <Home></Home>
+  )
+}
+
+export default App
---
+++ desacantik/src/components/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <div className='h-screen w-full bg-orange-300'></div>
+        <div className='h-96 bg-white w-full'></div>
+        <div className='bg-black bg-yellow`'></div>
+        <div className=""><div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Home
